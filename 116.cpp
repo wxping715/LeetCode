7fd137a5351c834c7aa0673047ecf9469291eeba
Xueping Weng <wxping715@users.noreply.github.com>
2016-03-28 00:46:40 +0800

    Update 116.cpp

--- 116.cpp
+++ 116.cpp
@@ -8,6 +8,7 @@
  */
 class Solution {
 public:
+    /* BFS Version */
     void connect(TreeLinkNode *root) {
         if (root == NULL) return;
         
@@ -28,5 +29,60 @@ public:
             if (cur->left != NULL) q.push(cur->left);
             if (cur->right != NULL) q.push(cur->right);
         }
+    }
+};
+
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+     /* Recursion Version */
+    void connect(TreeLinkNode *root) {
+        if (!root || !root->left || !root->right) 
+            return;
+        
+        root->left->next = root->right;
+        if (root->next)
+            root->right->next = root->next->left;
+        else
+            root->right->next = NULL;
+        
+        connect(root->left);
+        connect(root->right);
+    }
+};
+
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    /* Un-Recursion Version */
+    void connect(TreeLinkNode *root) {
+       for (TreeLinkNode* cur = root; cur; cur = cur->left) {
+            for (TreeLinkNode* next = cur; next; next = next->next) {
+                if (!next->left) break;
+                next->left->next = next->right;
+                
+                if (!next->next) 
+                    next->right->next = NULL;
+                else 
+                    next->righ   st->next = next->next->left;
+            }
+       }
     }
 };
