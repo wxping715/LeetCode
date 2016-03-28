9de6e3da3bc5017ba84908d5181b6150b03b9347
Xueping Weng <wxping715@users.noreply.github.com>
2016-03-28 20:04:20 +0800

    Update and rename 33.c to 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1, mid;
+        while (l <= r) {
+            mid = (r-l)/2 + l;
+            
+            if(nums[mid] == target) return mid;
+            if (nums[l] < nums[mid]) {
+                if (nums[l] <= target && target <= nums[mid]) r = mid - 1;
+                else l = mid + 1;
+            } else {
+                if (nums[mid+1] <= target && target <= nums[r]) l = mid + 1;
+                else r = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
