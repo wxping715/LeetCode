2f241ece6e2346c6a04ea7f335449d2257daf8b3
Xueping Weng <wxping715@users.noreply.github.com>
2016-04-05 23:27:15 +0800

    Create 319.cpp

---
+++ 319.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    /*
+    * k means the number of factors
+    * while i is perfect square number, k is even
+    * while i is not perfect square number, k is odd
+    */
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
