eb9817213aa106d9162cf6f7be3ee64c20d7638d
Xueping Weng <wxping715@users.noreply.github.com>
2016-04-05 23:35:00 +0800

    Update 268.cpp

--- 268.cpp
+++ 268.cpp
@@ -1,9 +1,28 @@
 class Solution {
 public:
+    /*
+    * math method
+    */
     int missingNumber(vector<int>& nums) {
         int sum = 0, n = nums.size();
         for (int i = 0; i < n; i++)
             sum += nums[i];
         return n*(n+1)/2 - sum;
+    }
+};
+
+class Solution {
+public:
+    /*
+    * bit compution
+    * 2n+1 numbers with one number is once, others are twice
+    */
+    int missingNumber(vector<int>& nums) {
+        int x = 0, n = nums.size();
+        for (int i = 0;i < n;i++) {
+            x ^= nums[i]^i;
+        }
+        x ^= n;
+        return x;
     }
 };
