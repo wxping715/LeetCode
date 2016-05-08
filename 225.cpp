4e188cef25275c1c735fa0a87b99ff01f7949866
Xueping Weng <wxping715@users.noreply.github.com>
2016-05-08 21:43:21 +0800

    Update 225.cpp

--- 225.cpp
+++ 225.cpp
@@ -1,60 +1,35 @@
 /*
-* using two queues to generate the stack
+* just using one queue to implement the stack, move the last size-1 element to the front to generate the stack
 */
 
 class Stack {
 public:
-    
-    queue<int> q1, q2;
-    int fq = 1;
-    
     // Push element x onto stack.
     void push(int x) {
-        if (fq == 1) q1.push(x);
-        else q2.push(x);
+        q.push(x);
+        int n = q.size();
+        //move the last val to the first position
+        for (int i = 1; i < n; i++) {
+            q.push(q.front());
+            q.pop();
+        }
     }
 
     // Removes the element on top of the stack.
     void pop() {
-        move();
-        if (fq == 1) q2.pop();
-        else q1.pop();
+        q.pop();
     }
 
     // Get the top element.
     int top() {
-        move();
-        int res;
-        if (fq == 1) {
-            res = q2.front();
-            q2.pop();
-            q1.push(res);
-        } else {
-            res = q1.front();
-            q1.pop();
-            q2.push(res);
-        }
-        return res;
+        return q.front();
     }
 
     // Return whether the stack is empty.
     bool empty() {
-        return (q1.empty() && q2.empty());
+        return q.empty();
     }
     
-    void move() {
-        if (q1.empty()) {
-            fq = 1;
-            while (q2.size() > 1) {
-                q1.push(q2.front());
-                q2.pop();
-            }
-        } else {
-            fq = 2;
-            while (q1.size() > 1) {
-                q2.push(q1.front());
-                q1.pop();
-            }
-        }
-    }
+private:
+    queue<int> q;
 };
