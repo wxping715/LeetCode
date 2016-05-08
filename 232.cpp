d079c8b22406827ead209df07cf0fb8478425487
Xueping Weng <wxping715@users.noreply.github.com>
2016-05-08 21:03:09 +0800

    Create 232.cpp

---
+++ 232.cpp
@@ -0,0 +1,33 @@
+class Queue {
+public:
+    
+    stack<int> s1, s2;
+    // Push element x to the back of queue.
+    void push(int x) {
+        s1.push(x);
+    }
+
+    // Removes the element from in front of queue.
+    void pop(void) {
+        if (s2.empty()) move();
+        s2.pop();
+    }
+
+    // Get the front element.
+    int peek(void) {
+        if (s2.empty()) move();
+        return s2.top();
+    }
+
+    // Return whether the queue is empty.
+    bool empty(void) {
+        return (s1.empty() && s2.empty());
+    }
+    
+    void move() {
+        while (!s1.empty()) {
+            s2.push(s1.top());
+            s1.pop();
+        }
+    }
+};
