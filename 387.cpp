07721d9fa1b6e19760d1e4f4695cecbfbeab11a6
Xueping Weng <wxping715@users.noreply.github.com>
2016-09-29 20:17:35 +0800

    Create 387.cpp

---
+++ 387.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int freq[26] = {0};
+        for (int i = 0; i < s.length(); i++)
+            freq[s[i]-'a']++;
+        for (int i = 0; i < s.length(); i++)
+            if (freq[s[i]-'a'] == 1) return i;
+        return -1;
+    }
+};
