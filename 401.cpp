341c3104c18d791a4160d469e79c8e4624d900d7
Xueping Weng <wxping715@users.noreply.github.com>
2016-10-06 01:09:37 +0800

    Update 401.cpp
    Add bitset solution

--- 401.cpp
+++ 401.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
-
+    
+    // bit set solution
+    vector<string> readBinaryWatch(int num) {
+        vector<string> res;
+        if (num > 8 || num < 0) return res;
+        
+        for (int i = 0; i < 12; i++)
+            for (int j = 0; j < 60; j++) {
+                if (bitset<16>((j << 4) + i).count() == num) {
+                    string time = to_string(i);
+                    time.push_back(':');
+                    if (j < 10) time.push_back('0');
+                    time += to_string(j);
+                    
+                    res.push_back(time);
+                }
+            }
+        return res;
+    }
+    
+    //dfs solution
     vector<string> readBinaryWatch(int num) {
         vector<string> res;
         if (num > 8 || num < 0) return res;
