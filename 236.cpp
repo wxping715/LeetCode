64b3afa5871b523fb4aefd1e511b24631d26351b
Xueping Weng <wxping715@users.noreply.github.com>
2016-11-29 13:00:05 +0800

    Update 236.cpp

--- 236.cpp
+++ 236.cpp
@@ -1,31 +1,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (p == NULL) return q;
-        if (q == NULL) return p;
-        if (root == NULL) return NULL;
+        if (!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
         
-        unordered_map<TreeNode*, TreeNode*> parent_map;
-        dfs(root, NULL, parent_map);
-        
-        unordered_set<TreeNode *> p1;
-        while (p != NULL) {
-            p1.insert(p);
-            p = parent_map[p];
-        }
-        
-        while (q != NULL) {
-            if (p1.find(q) != p1.end()) return q;
-            q = parent_map[q];
-        }
-        return NULL;
-    }
-    
-    void dfs(TreeNode* root, TreeNode *pre, unordered_map<TreeNode*, TreeNode*> &parent_map) {
-        if (root == NULL) return;
-        parent_map[root] = pre;
-        pre = root;
-        dfs(root->left, pre, parent_map);
-        dfs(root->right, pre, parent_map);
+        if (left && right) return root;
+        return left ? left : right;
     }
 };
