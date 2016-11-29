1b6960115f584fda6e782f858bb1525b6f3533bf
Xueping Weng <wxping715@users.noreply.github.com>
2016-11-29 19:27:29 +0800

    Update 300.cpp

--- 300.cpp
+++ 300.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
-
     /*
     * O(n^2)
-    * dp[i] represents the length of LIS ending with nums[i]
-    * dp[i] = max(dp[j]+1) while 0 <= j < i and nums[i] > nums[j]
     */
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), 1);
+    // int lengthOfLIS(vector<int>& nums) {
+    //     if (nums.empty()) return 0;
         
-        for (int i = 1; i < dp.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i] && dp[i] < dp[j]+1)
-                    dp[i] = dp[j] + 1;
-            }
+    //     vector<int> dp(nums.size(), 1);
+    //     for (int i = 0; i < nums.size(); i++) {
+    //         for (int j = 0; j < i; j++)
+    //             if (nums[j] < nums[i])
+    //                 dp[i] = max(dp[i], dp[j]+1);
+    //     }
+        
+    //     int maxl = 1;
+    //     for (int i = 0; i < nums.size(); i++)
+    //         if (maxl < dp[i])
+    //             maxl = dp[i];
+    //     return maxl;
+    // }
+    
+    /**
+     * O(nlogn)
+    **/
+    int lengthOfLIS(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int len = 0;
+        for (int n : nums) {
+            int pos = lower_bound(dp.begin(), dp.begin()+len, n) - dp.begin();
+            dp[pos] = n;
+            len = max(len, pos+1);
         }
         
-        int res = 0;
-        for (int i = 0; i < dp.size(); i++) {
-            if (res < dp[i]) res = dp[i];
-        } 
-        return res;
+        return len;
     }
 };
-
