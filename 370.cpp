04c8ea60eeef406d9b2d174b9ce1e013ae6e66fc
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-14 22:36:10 +0800

    Create 370.cpp

---
+++ 370.cpp
@@ -0,0 +1,61 @@
+/*Assume you have an array of length n initialized with all 0's and are given k update operations.
+
+Each operation is represented as a triplet: [startIndex, endIndex, inc] which increments each element of subarray A[startIndex ... endIndex] (startIndex and endIndex inclusive) with inc.
+
+Return the modified array after all k operations were executed.
+
+Example:
+
+Given:
+
+    length = 5,
+    updates = [
+        [1,  3,  2],
+        [2,  4,  3],
+        [0,  2, -2]
+    ]
+
+Output:
+
+    [-2, 0, 3, 5, 3]
+Explanation:
+
+Initial state:
+[ 0, 0, 0, 0, 0 ]
+
+After applying operation [1, 3, 2]:
+[ 0, 2, 2, 2, 0 ]
+
+After applying operation [2, 4, 3]:
+[ 0, 2, 5, 5, 3 ]
+
+After applying operation [0, 2, -2]:
+[-2, 0, 3, 5, 3 ]
+Hint:
+
+Thinking of using advanced data structures? You are thinking it too complicated.
+For each update operation, do you really need to update all elements between i and j?
+Update only the first and end element is sufficient.
+The optimal time complexity is O(k + n) and uses O(1) extra space.
+Credits:
+Special thanks to @vinod23 for adding this problem and creating all test cases.*/
+
+
+class Solution {
+public:
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        vector<int> res(length, 0);
+        for (auto update : updates) {
+            res[update[0]] += update[2];
+            if (update[1]+1 < length)  
+                res[update[1]+1] -= update[2];
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < length; i++) {
+            sum += res[i];
+            res[i] = sum;
+        }
+        return res;
+    }
+};
