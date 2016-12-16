a10646996775b3c5c9ea67eeb10777601782362d
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-16 13:46:57 +0800

    Create 314.cpp

---
+++ 314.cpp
@@ -0,0 +1,88 @@
+/*Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+Examples:
+
+Given binary tree [3,9,20,null,null,15,7],
+   3
+  /\
+ /  \
+ 9  20
+    /\
+   /  \
+  15   7
+return its vertical order traversal as:
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+Given binary tree [3,9,8,4,0,1,7],
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+return its vertical order traversal as:
+[
+  [4],
+  [9],
+  [3,0,1],
+  [8],
+  [7]
+]
+Given binary tree [3,9,8,4,0,1,7,null,null,null,2,5] (0's right child is 2 and 1's left child is 5),
+     3
+    /\
+   /  \
+   9   8
+  /\  /\
+ /  \/  \
+ 4  01   7
+    /\
+   /  \
+   5   2
+return its vertical order traversal as:
+[
+  [4],
+  [9,5],
+  [3,0,1],
+  [8,2],
+  [7]
+]*/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        map<int, vector<int>> records;
+        vector<vector<int>> res;
+        queue<pair<TreeNode*, int>> q;
+        if (root != NULL)
+            q.push(make_pair(root, 0));
+        while (!q.empty()) {
+            auto cur = q.front();
+            q.pop();
+            records[cur.second].push_back(cur.first->val);
+            if (cur.first->left) q.push(make_pair(cur.first->left, cur.second-1));
+            if (cur.first->right) q.push(make_pair(cur.first->right, cur.second+1));
+        }
+
+        for (auto it : records)
+            res.push_back(it.second);
+        return res;
+    }
+};
