3ce878c7635c0d04c72c59df6bd2e783da1f9651
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-16 22:47:18 +0800

    Create 359.cpp

---
+++ 359.cpp
@@ -0,0 +1,65 @@
+/*Design a logger system that receive stream of messages along with its timestamps, each message should be printed if and only if it is not printed in the last 10 seconds.
+
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed in the given timestamp, otherwise returns false.
+
+It is possible that several messages arrive roughly at the same time.
+
+Example:
+
+Logger logger = new Logger();
+
+// logging string "foo" at timestamp 1
+logger.shouldPrintMessage(1, "foo"); returns true; 
+
+// logging string "bar" at timestamp 2
+logger.shouldPrintMessage(2,"bar"); returns true;
+
+// logging string "foo" at timestamp 3
+logger.shouldPrintMessage(3,"foo"); returns false;
+
+// logging string "bar" at timestamp 8
+logger.shouldPrintMessage(8,"bar"); returns false;
+
+// logging string "foo" at timestamp 10
+logger.shouldPrintMessage(10,"foo"); returns false;
+
+// logging string "foo" at timestamp 11
+logger.shouldPrintMessage(11,"foo"); returns true;*/
+
+
+class Logger {
+public:
+    /** Initialize your data structure here. */
+    Logger() {
+        
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    bool shouldPrintMessage(int timestamp, string message) {
+        while (!mqueue.empty() && mqueue.front().first+10 <= timestamp) {
+            auto cur = mqueue.front();
+            if (printmap.find(cur.second) != printmap.end() && printmap[cur.second] == cur.first)
+                printmap.erase(cur.second);
+            mqueue.pop_front();
+        }
+        
+        mqueue.push_back(make_pair(timestamp, message));
+        if (printmap.find(message) == printmap.end()) {
+            printmap[message] = timestamp;
+            return true;
+        }
+        return false;
+    }
+    
+private:
+    deque<pair<int, string>> mqueue;
+    unordered_map<string, int> printmap;
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * bool param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
