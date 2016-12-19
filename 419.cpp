d5b734e37d5c66df06cffd5985c2efe5309529fd
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-19 20:06:33 +0800

    Create 419.cpp

---
+++ 419.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        if (board.size() == 0 || board[0].size() == 0) return 0;
+        
+        int ret = 0;
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == 'X' && (i == 0 || board[i-1][j] != 'X') && (j == 0 || board[i][j-1] != 'X'))
+                    ret++;
+            }
+        return ret;
+    }
+};
