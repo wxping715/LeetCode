5e1edf565ac4216756ed251b35b7b64817ac1fab
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-29 23:43:44 +0800

    Create 56.cpp

---
+++ 56.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), cmp);
+        vector<Interval> ret;
+        if (intervals.empty()) return ret;
+        
+        int s = intervals[0].start, e = intervals[0].end;
+        for (Interval interval : intervals) {
+            if (interval.start > e ) {
+                ret.push_back(Interval(s,e));
+                s = interval.start;
+                e = interval.end;
+            } else {
+                s = min(interval.start, s);
+                e = max(interval.end, e);
+            }
+        }
+        ret.push_back(Interval(s, e));
+        return ret;
+    }
+    
+    bool static cmp(const Interval& i1, const Interval& i2) {
+        if (i1.start == i2.start) return i1.end < i2.end;
+        return i1.start < i2.start;
+    }
+};
