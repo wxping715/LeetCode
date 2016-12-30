8e45427766b677a55a77d50d9786562c624d5bc4
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-30 12:20:56 +0800

    Update 253.cpp

--- 253.cpp
+++ 253.cpp
@@ -1,3 +1,9 @@
+/*Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+
+For example,
+Given [[0, 30],[5, 10],[15, 20]],
+return 2.
+*/
 /**
  * Definition for an interval.
  * struct Interval {
@@ -9,6 +15,8 @@
  */
 class Solution {
 public:
+
+    //solution1
     int minMeetingRooms(vector<Interval>& intervals) {
         int ret = 0, cur = 0;
         priority_queue<int> q;
@@ -25,8 +33,43 @@ public:
         return ret;
     }
     
+    // solution 2
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int ret = 0, cur = 0;
+        priority_queue<int> q;
+        sort(intervals.begin(), intervals.end(), cmp);
+        if (intervals.empty()) return 0;
+        
+        q.push(-intervals[0].end);
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i].start >= -q.top())
+                q.pop();
+            q.push(-intervals[i].end);
+        }
+        return q.size();
+    }
+    
+    //solution 3
+    int minMeetingRooms(vector<Interval>& intervals) {
+        vector<int> starts, ends;
+        for (Interval interval : intervals) {
+            starts.push_back(interval.start);
+            ends.push_back(interval.end);
+        }
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+        
+        int eidx = 0, ret = 0;
+        for (int i = 0; i < starts.size(); i++) {
+            if (starts[i] < ends[eidx]) ret++;
+            else eidx++;
+        }
+        return ret;
+    }
+    
     bool static cmp(const Interval& i1, const Interval& i2) {
         if (i1.start == i2.start) return i1.end < i2.end;
         return i1.start < i2.start;
     }
 };
+
