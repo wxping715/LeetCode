192c9ec3a90d0d19b6d4d774d647b46e77a8f014
Xueping Weng <wxping715@users.noreply.github.com>
2016-12-30 09:50:10 +0800

    Update 257.cpp

--- 257.cpp
+++ 257.cpp
@@ -1,40 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
 
-    using Elem = pair<TreeNode*, string>;
-    using Stack = stack<Elem>;
-
+    // dfs
+    // vector<string> binaryTreePaths(TreeNode* root) {
+    //     vector<string> ret, path;
+    //     if (!root) return ret;
+    //     generate(root, path, ret);
+    //     return ret;
+    // }
+    
+    // void generate(TreeNode* root, vector<string>& path, vector<string>& ret) {
+    //     path.push_back(to_string(root->val));
+    //     if (!root->left && !root->right) {
+    //         string pstr = "";
+    //         for (int i = 0; i < path.size(); i++) {
+    //             if (i != 0) pstr += "->";
+    //             pstr += path[i];
+    //         }
+    //         ret.push_back(pstr);
+    //     }
+        
+    //     if (root->left) generate(root->left, path, ret);
+    //     if (root->right) generate(root->right, path, ret);
+    //     path.pop_back();
+    // }
+    
+    //iterival
     vector<string> binaryTreePaths(TreeNode* root) {
-
-        vector<string> paths;
-
-        if(root)
-        {
-            Stack pool;
-            pool.push(Elem(root, to_string(root->val)));
-
-            while(!pool.empty())
-            {
-                auto p = pool.top();
-                pool.pop();
-
-                if(!p.first->left && !p.first->right)
-                    paths.push_back(p.second);
-
-                if(p.first->left)
-                {
-                    string nstring = p.second + "->" + to_string(p.first->left->val);
-                    pool.push(Elem(p.first->left, nstring));
-                }
-
-                if(p.first->right)
-                {
-                    string nstring = p.second + "->" + to_string(p.first->right->val);
-                    pool.push(Elem(p.first->right, nstring));
-                }
+        stack<TreeNode*> s;
+        stack<string> path;
+        vector<string> ret;
+        if (!root) return ret;
+        
+        s.push(root);
+        path.push("");
+        while (!s.empty()) {
+            auto cur = s.top(); s.pop();
+            string cpath = path.top(); path.pop();
+            if (!cur->left && !cur->right) ret.push_back(cpath+to_string(cur->val));
+            if (cur->right) {
+                s.push(cur->right);
+                path.push(cpath+to_string(cur->val)+"->");
+            }
+            if (cur->left) {
+                s.push(cur->left);
+                path.push(cpath+to_string(cur->val)+"->");
             }
         }
-
-        return paths;
-    }
+        return ret;
+     }
 };
