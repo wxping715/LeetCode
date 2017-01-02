78731a17ce6ef12d812460305bf066ff6d24d779
Xueping Weng <wxping715@users.noreply.github.com>
2017-01-02 22:42:09 +0800

    Update 114.cpp

--- 114.cpp
+++ 114.cpp
@@ -9,50 +9,22 @@
  */
 class Solution {
 public:
-    /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
- 
-struct TreeNode* dfs(struct TreeNode *root,struct TreeNode *& rear){
-    
-    if(root == NULL) return NULL;
-    if(root->left == NULL && root->right == NULL){
-        rear = root;
-        return root;  
-    } 
-    
-    struct TreeNode *left, *right, *lrear, *rrear, *p, *q;
-    left = dfs(root->left,lrear);
-    right = dfs(root->right,rrear);
-    
-    if(left == NULL){
-        rear = rrear;
-        root->left = NULL;
-        root->right = right;
-        return root;
-    }else if(right == NULL){
-        rear = lrear;
-        root->right = left;
-        root->left = NULL;
-        return root;
-    }else{
-        p = right;
-        rear = rrear;
-        root->right = left;
-        root->left = NULL;
-        lrear->right = right;
-        return root;
+    void flatten(TreeNode* root) {
+        stack<TreeNode*> s;
+        if (!root) return;
+        s.push(root);
+        
+        TreeNode* rear = NULL;
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
+            s.pop();
+            if (cur->right) s.push(cur->right);
+            if (cur->left) s.push(cur->left);
+            
+            cur->left = NULL;
+            cur->right = NULL;
+            if (rear) rear->right = cur;
+            rear = cur;
+        }
     }
-}
-
-void flatten(struct TreeNode* root) {
-    struct TreeNode *rear = NULL;
-    root = dfs(root,rear);
-}
-
 };
