c01236dcd5f58a137947bccfd12b60129ed42f83
Xueping Weng <wxping715@users.noreply.github.com>
2017-01-03 11:08:50 +0800

    Update 75.cpp

--- 75.cpp
+++ 75.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        int r = 0, w = 0, b = 0;
-        for(int i = 0;i < n;i++){
-            if(A[i] == 0) r++;
-            else if(A[i] == 1) w++;
-            else b++;
+
+    //two pass
+    // void sortColors(vector<int>& nums) {
+    //     int cnt[3] = {0};
+    //     for (int num : nums)
+    //         cnt[num]++;
+    //     int k = 0;
+    //     for (int i = 0;i < 3; i++) {
+    //         while (cnt[i]--)
+    //             nums[k++] = i;
+    //     }
+    // }
+    
+    //one pass
+    void sortColors(vector<int>& nums) {
+        int zero = 0, two = nums.size()-1;
+        for (int i = 0; i <= two; i++) {
+            while (nums[i] == 2 && i < two) swap(nums[i], nums[two--]);
+            while (nums[i] == 0 && i > zero) swap(nums[i], nums[zero++]);
         }
-        int i = 0;
-        while(r--) A[i++] = 0;
-        while(w--) A[i++] = 1;
-        while(b--) A[i++] = 2;
     }
 };
