db547f66314bcf84dcf37ae47b57636e2079de7a
Xueping Weng <wxping715@users.noreply.github.com>
2017-01-12 23:46:24 +0800

    Update 326.cpp

--- 326.cpp
+++ 326.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    /*
-    * 1. the general method is to divide 3 continuly.
-    * 2. the param is integer, so the maximul which is power of three is 3^19=1162261467, if num is power of three, then 3^19 % num == 0;
-    */
     bool isPowerOfThree(int n) {
-        if (n <= 0) return false;
-        
-        const int m = 1162261467;
-        return m % n == 0;
+        return (n > 0 && 1162261467 % n == 0);
     }
 };
