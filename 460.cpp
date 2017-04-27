99746a76f9ec9c1fe18f98919c3caa8c643dcfb5
Xueping Weng <wxping715@users.noreply.github.com>
2017-04-28 00:10:44 +0800

    Update 460.cpp

--- 460.cpp
+++ 460.cpp
@@ -1,40 +1,67 @@
-class LRUCache{
+class LFUCache {
 public:
-    LRUCache(int capacity) {
+    LFUCache(int capacity) {
+        size = 0;
         this->capacity = capacity;
     }
     
     int get(int key) {
-        auto it = cache.find(key);
-        if (it == cache.end()) return -1;
+        if (!cache.count(key)) return -1;
         
-        int val = it->second->second;
-        ulist.splice(ulist.begin(), ulist, it->second);
-        return val;
+        int f = cache[key].second;
+        freqMap[f].erase(ptrMap[key]);
+        cache[key].second ++;
+        freqMap[f+1].push_back(key);
+        ptrMap[key] = --freqMap[f+1].end();
+        
+        if (freqMap[minFreq].size() == 0)
+            minFreq++;
+        
+        return cache[key].first;
     }
     
-    void set(int key, int value) {
-        auto it = cache.find(key);
-        if (it != cache.end()) {
-            ulist.splice(ulist.begin(), ulist, it->second);
-            it->second->second = value;
-        } else {
-            if (ulist.size() == capacity)
-                remove();
-            ulist.emplace_front(key, value);
-            cache.emplace(key, ulist.begin()); 
+    void put(int key, int value) {
+        if (capacity <= 0) return;
+        
+        int curVal = get(key);
+        if (curVal != -1) {
+            cache[key].first = value;
+            return;
         }
+        
+        if (size >= capacity) {
+            int dkey = freqMap[minFreq].front();
+            cache.erase(dkey);
+            ptrMap.erase(dkey);
+            freqMap[minFreq].pop_front();
+            size--;
+        }
+        
+        cache[key] = make_pair(value, 1);
+        freqMap[1].push_back(key);
+        ptrMap[key] = --freqMap[1].end();
+        minFreq = 1;
+        size++;
     }
     
-    void remove() {
-        auto node = ulist.back();
-        cache.erase(node.first);
-        ulist.pop_back();
-    }
-
+    
 private:
     typedef pair<int, int> node;
-    list<node> ulist;
-    unordered_map<int, list<node>::iterator> cache;
+    // key -> list pointer
+    unordered_map<int, list<int>::iterator> ptrMap;
+    // key -> val, freq
+    unordered_map<int, node> cache;
+    // freq -> list<key>
+    unordered_map<int, list<int>> freqMap;
+    
+    int size;
     int capacity;
+    int minFreq;
 };
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.set(key,value);
+ */
