c005c45dcb64ec019fe26761056b0a8da29433d7
Xueping Weng <wxping715@users.noreply.github.com>
2017-04-27 10:40:48 +0800

    Create 561.cpp

---
+++ 561.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        for (int i = 0; i < nums.size(); i += 2)
+            res += min(nums[i+1], nums[i]);
+        return res;
+    }
+};
