e8690c2adad911554a4a33c850fa8bdcb4accdf4
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-08 11:08:32 +0800

    Update 258.cpp

--- 258.cpp
+++ 258.cpp
@@ -1,5 +1,10 @@
-int addDigits(int num) {
-    if (num == 0) return 0;
-    if (num % 9 == 0) return 9;
-    return num % 9;
-}
+class Solution {
+public:
+    
+    // a*1000 + b*100 + c*10 + d = (a+b+c+d) + (a*999 + b*99 + c*9)
+    // the right part is always be moded by 9
+    int addDigits(int num) {
+        int r = num%9;
+        return r ? r : 9;
+    }
+};
