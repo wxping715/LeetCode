c38c91d5939466f2de0581967dda09770df4036a
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-27 21:28:15 +0800

    Update 245.cpp

--- 245.cpp
+++ 245.cpp
@@ -1,20 +1,30 @@
+/*
+This is a follow up of Shortest Word Distance. The only difference is now word1 could be the same as word2.
+
+Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+
+word1 and word2 may be the same and they represent two individual words in the list.
+
+For example,
+Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+
+Given word1 = “makes”, word2 = “coding”, return 1.
+Given word1 = "makes", word2 = "makes", return 3.
+
+Note:
+You may assume word1 and word2 are both in the list.
+*/
+
 class Solution {
 public:
     int shortestWordDistance(vector<string>& words, string word1, string word2) {
-        int idx1 = -1, idx2 = -1, res = words.size();
-        bool first = true;
-        for (int i = 0; i < words.size(); i++) {
-            if (words[i] == word1 && (word1 != word2 || first)) {
-                idx1 = i;
-                first = false;
+        int i = -1, j = 0, res = words.size();
+        for (; j < words.size(); j++) {
+            if (word1 == words[j] || word2 == words[j]) {
+                if (i != -1 && (word1 == word2 || words[i] != words[j]))
+                    res = min(res, j-i);
+                i = j;
             }
-            else if (words[i] == word2 && (word1 != word2 || !first)) {
-                idx2 = i;
-                first = true;
-            }
-            
-            if (idx1 != -1 && idx2 != -1)
-                res = min(res, abs(idx1-idx2));
         }
         return res;
     }
