1a64e16b34586991256a9313c8b1d8d9db9728f2
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-28 13:49:14 +0800

    Update 162.cpp

--- 162.cpp
+++ 162.cpp
@@ -1,13 +1,33 @@
+/*
+A peak element is an element that is greater than its neighbors.
+
+Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+
+The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+
+You may imagine that num[-1] = num[n] = -∞.
+
+For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+
+click to show spoilers.
+
+Credits:
+Special thanks to @ts for adding this problem and creating all test cases.
+*/
+
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        vector<bool> inc(nums.size(), false);
-        for (int i = 0; i < nums.size(); i++)
-            if (i == 0 || nums[i] > nums[i-1])
-                inc[i] = true;
-        for (int i = nums.size()-1; i >= 0; i--)
-            if ((i == nums.size()-1 || nums[i] > nums[i+1]) && inc[i])
-                return i;
-        return -1;
+        int l = 0, r = nums.size()-1;
+        while (l < r) {
+            // m <= r
+            int m = (r+l)/2;
+            
+            if (nums[m] < nums[m+1])
+                l = m+1;
+            else
+                r = m;
+        }
+        return l;
     }
 };
