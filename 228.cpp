0a00ca4ec5d4ee3ce73c11d97916b3a6e58054fa
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-30 15:44:53 +0800

    Update 228.cpp

--- 228.cpp
+++ 228.cpp
@@ -1,30 +1,31 @@
+/*
+Given a sorted integer array without duplicates, return the summary of its ranges.
+
+For example, given [0,1,2,4,5,7], return ["0->2","4->5","7"].
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        if (nums.empty()) return res;
         
-        vector<string> v;
-        if(nums.size() == 0) return v;
-        
-        int s = nums[0],r = s;
-        for(int i = 1;i < nums.size();i++){
-            if(nums[i] != (r+1)){
-                v.push_back(get_range(s,r));
+        int s = nums[0], e = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (e+1 == nums[i])
+                e++;
+            else {
+                if (s == e) res.push_back(to_string(s));
+                else res.push_back(to_string(s)+"->"+to_string(e));
+                
                 s = nums[i];
+                e = nums[i];
             }
-            r = nums[i];
         }
-        v.push_back(get_range(s,r));
-        return v;
-    }
-    
-    string get_range(int s, int r){
-        stringstream ss;
-        ss<<s;
-        string s_str = ss.str();
-        ss.str("");
-        
-        if(s == r) return s_str;
-        ss<<r;
-        return s_str+"->"+ss.str();
+        res.push_back(s == e ? to_string(s): to_string(s)+"->"+to_string(e));
+        return res;
     }
 };
