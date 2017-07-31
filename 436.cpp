8250787d042346c5cf53cc1a48bf62008b5625e6
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-31 11:10:05 +0800

    Update 436.cpp

--- 436.cpp
+++ 436.cpp
@@ -1,3 +1,35 @@
+/*
+Given a set of intervals, for each of the interval i, check if there exists an interval j whose start point is bigger than or equal to the end point of the interval i, which can be called that j is on the "right" of i.
+
+For any interval i, you need to store the minimum interval j's index, which means that the interval j has the minimum start point to build the "right" relationship for interval i. If the interval j doesn't exist, store -1 for the interval i. Finally, you need output the stored value of each interval as an array.
+
+Note:
+You may assume the interval's end point is always bigger than its start point.
+You may assume none of these intervals have the same start point.
+Example 1:
+Input: [ [1,2] ]
+
+Output: [-1]
+
+Explanation: There is only one interval in the collection, so it outputs -1.
+Example 2:
+Input: [ [3,4], [2,3], [1,2] ]
+
+Output: [-1, 0, 1]
+
+Explanation: There is no satisfied "right" interval for [3,4].
+For [2,3], the interval [3,4] has minimum-"right" start point;
+For [1,2], the interval [2,3] has minimum-"right" start point.
+Example 3:
+Input: [ [1,4], [2,3], [3,4] ]
+
+Output: [-1, 2, -1]
+
+Explanation: There is no satisfied "right" interval for [1,4] and [3,4].
+For [2,3], the interval [3,4] has minimum-"right" start point.
+*/
+
+
 /**
  * Definition for an interval.
  * struct Interval {
@@ -7,24 +39,21 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
+
 class Solution {
 public:
-    vector<int> findRightInterval(vector<Interval>& intervals) {
-        vector<int> lps;
-        unordered_map<int, int> index;
-        
-        for (int i = 0; i < intervals.size(); i++) {
-            index[intervals[i].start] = i;
-            lps.push_back(intervals[i].start);
-        }
-        sort(lps.begin(), lps.end());
-        
-        vector<int> res;
-        for (int i = 0; i < intervals.size(); i++) {
-            vector<int>::iterator it = lower_bound(lps.begin(), lps.end(), intervals[i].end);
-            if (it == lps.end()) res.push_back(-1);
-            else res.push_back(index[*it]);
-        }
-        return res;
-    }
+	
+	vector<int> findRightInterval(vector<Interval>& intervals) {
+		map<int, int> imap;
+		for (int i = 0; i < intervals.size(); i++)
+			imap[intervals[i].start] = i;
+		
+		vector<int> res;
+		for (Interval interval : intervals) {
+			auto it = imap.lower_bound(interval.end);
+			if (it == imap.end()) res.push_back(-1);
+			else res.push_back(it->second);
+		}
+		return res;
+	}
 };
