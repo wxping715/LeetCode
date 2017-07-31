093bb80589b9545be78e3f67d1500ebe1ce0cab1
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-31 12:28:56 +0800

    Update 69.cpp

--- 69.cpp
+++ 69.cpp
@@ -1,7 +1,22 @@
+/*
+Implement int sqrt(int x).
+
+Compute and return the square root of x.
+
+*/
+
 class Solution {
 public:
-    int majorityElement(vector<int> &num) {
-        sort(num.begin(),num.end());
-        return num[num.size()/2];
+    int mySqrt(int x) {
+        typedef long long int LL;
+        
+        LL l = 0, r = min(x, 50000);
+        while (l <= r) {
+            LL m = (r+l)/2;
+            if (m*m <= x && (m+1)*(m+1) > x) return m;
+            else if (m*m >= x) r = m-1;
+            else l = m+1;
+        }
+        return 0;
     }
 };
