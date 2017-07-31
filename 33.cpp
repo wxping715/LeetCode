47c551ebbbd1af83f42669d9ebd020f8e7b0c238
Xueping Weng <wxping715@users.noreply.github.com>
2017-07-31 14:15:47 +0800

    Update 33.cpp

--- 33.cpp
+++ 33.cpp
@@ -1,18 +1,28 @@
+/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+*/
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1, mid;
+        int l = 0, r = nums.size()-1;
         while (l <= r) {
-            mid = (r-l)/2 + l;
-            
-            if(nums[mid] == target) return mid;
-            if (nums[l] < nums[mid]) {
-                if (nums[l] <= target && target <= nums[mid]) r = mid - 1;
-                else l = mid + 1;
-            } else {
-                if (nums[mid+1] <= target && target <= nums[r]) l = mid + 1;
-                else r = mid - 1;
-            }
+        	int m = (l+r)/2;
+        	if (nums[m] == target) return m;
+            // case 1: should search in right
+        	else if (nums[l] <= nums[m] && nums[l] <= target && nums[m] >= target)
+        		r = m-1;
+            // case 2: should search in right
+        	else if (nums[m] <= nums[r] && (nums[m] > target || nums[r] < target))
+        		r = m-1;
+        	else
+        		l = m+1;
         }
         return -1;
     }
