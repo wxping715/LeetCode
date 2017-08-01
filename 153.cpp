d4b6cb4f45144903f9818db45aabca2b37d42c74
Xueping Weng <wxping715@users.noreply.github.com>
2017-08-01 16:36:57 +0800

    Update and rename 153.c to 153.cpp

--- 153.cpp
+++ 153.cpp
@@ -13,12 +13,9 @@ public:
     int findMin(vector<int>& nums) {
         int l = 0, r = nums.size()-1;
         while (l < r) {
-        	if (nums[l] < nums[r])
-        		return nums[l];
-        	
             int m = (l+r)/2;
         	// must be increase, decrese
-        	if (nums[l] <= nums[m]) l = m+1;
+        	if (nums[m] > nums[r]) l = m+1;
         	else r = m;
         }
         return nums[l];
