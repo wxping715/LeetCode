9c2711515fa2ac1bae314490205ab3ca9d406afe
Xueping Weng <wxping715@users.noreply.github.com>
2017-08-01 09:40:01 +0800

    Create 374.cpp

---
+++ 374.cpp
@@ -0,0 +1,38 @@
+/*
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I'll tell you whether the number is higher or lower.
+
+You call a pre-defined API guess(int num) which returns 3 possible results (-1, 1, or 0):
+
+-1 : My number is lower
+ 1 : My number is higher
+ 0 : Congrats! You got it!
+Example:
+n = 10, I pick 6.
+
+Return 6.
+*/
+
+
+// Forward declaration of guess API.
+// @param num, your guess
+// @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+int guess(int num);
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n, m;
+        while (l <= r) {
+            m = l+(r-l)/2;
+            int f = guess(m);
+            if (f == 0) return m;
+            else if (f == -1) r = m-1;
+            else l = m+1;
+        }
+        return -1;
+    }
+};
