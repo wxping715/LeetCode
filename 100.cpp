a8ce19dfaa5a8e4b39b735d25229ebd653ae68ac
Xueping Weng <wxping715@users.noreply.github.com>
2017-08-01 17:03:24 +0800

    Update and rename 100.c to 100.cpp

--- 100.cpp
+++ 100.cpp
@@ -1,3 +1,9 @@
+/*
+Given two binary trees, write a function to check if they are equal or not.
+
+Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+*/
+
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -10,7 +16,6 @@ bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
     
     if(p == NULL && q == NULL) return true;
     if(p != NULL && q != NULL){
-
         if(p->val != q->val) return false;
         if(!isSameTree(p->left,q->left)) return false;
         if(!isSameTree(p->right,q->right)) return false;
