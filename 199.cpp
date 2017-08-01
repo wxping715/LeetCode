461fc74599714c93d426dad3b7dd449674fa8beb
Xueping Weng <wxping715@users.noreply.github.com>
2017-08-01 22:08:43 +0800

    Update 199.cpp

--- 199.cpp
+++ 199.cpp
@@ -1,5 +1,18 @@
+/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4].
+*/
+
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -9,23 +22,35 @@
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode *root) {
-        
-        pair<TreeNode*,int> p;
-        queue<pair<TreeNode *,int> > q;
+    void dfs(TreeNode* root, int lv, vector<int> &res){
+        if(!root)   return;
+        if(lv>=res.size()) res.push_back(root->val);
+        dfs(root->right,lv+1,res);
+        dfs(root->left,lv+1,res);
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
         vector<int> res;
-        if(root == NULL) return res;
+        dfs(root, 0, res);
+        return res;
+    }
+    
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        vector<int> res;
         
-        q.push(make_pair(root,1));
-        while(!q.empty())
-        {
-            p = q.front();
-            q.pop();
-            if(q.empty() || q.front().second != p.second)
-            res.push_back(p.first->val);        
-            
-            if(p.first->left != NULL) q.push(make_pair(p.first->left,p.second+1));
-            if(p.first->right != NULL) q.push(make_pair(p.first->right,p.second+1));
+        if (!root) return res;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            res.push_back(q.front()->val);
+            while (n--) {
+                auto cur = q.front();
+                q.pop();
+                
+                if (cur->right) q.push(cur->right);
+                if (cur->left) q.push(cur->left);
+            }
         }
         return res;
     }
