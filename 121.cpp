252fa3a85529313832e1290f48639dcc22242ed0
Xueping Weng <wxping715@users.noreply.github.com>
2017-08-01 12:42:36 +0800

    Update 121.cpp

--- 121.cpp
+++ 121.cpp
@@ -1,11 +1,28 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Example 1:
+Input: [7, 1, 5, 3, 6, 4]
+Output: 5
+
+max. difference = 6-1 = 5 (not 7-1 = 6, as selling price needs to be larger than buying price)
+Example 2:
+Input: [7, 6, 4, 3, 1]
+Output: 0
+
+In this case, no transaction is done, i.e. max profit = 0.
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max_price = -0x3fffffff, max_profit = 0;
-        for (int i = prices.size()-1;i >= 0; i--) {
-            max_price = max(max_price, prices[i]);
-            max_profit = max(max_profit, max_price - prices[i]);
+        int ret = 0, minprice = INT_MAX;
+        for (int price : prices) {
+            ret = max(ret, price - minprice);
+            minprice = min(price, minprice);
         }
-        return max_profit;
+        return ret;
     }
 };
