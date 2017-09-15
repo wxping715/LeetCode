160979759883c607beca687b55230703d1554ddb
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-15 12:32:43 +0800

    Update 238.cpp

--- 238.cpp
+++ 238.cpp
@@ -1,20 +1,30 @@
+/*
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+*/
+
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> ret(nums.size(), 0);
-        
+        vector<int> r(nums.size(),1);
         int p = 1;
-        ret[0] = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            p *= nums[i-1];
-            ret[i] = p;
+        for (int i = 0; i < nums.size(); i++) {
+            r[i] = p;
+            p *= nums[i];
         }
         
         p = 1;
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            p *= nums[i+1];
-            ret[i] *= p;
+        for (int i = nums.size()-1; i >= 0; i--) {
+            r[i] *= p;
+            p *= nums[i];
         }
-        return ret;
+        
+        return r;
     }
 };
