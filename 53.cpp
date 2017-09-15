e0145390d5eb9f768518a1f0317a240826d82b05
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-15 12:36:21 +0800

    Update 53.cpp

--- 53.cpp
+++ 53.cpp
@@ -4,19 +4,19 @@ Find the contiguous subarray within an array (containing at least one number) wh
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
 the contiguous subarray [4,-1,2,1] has the largest sum = 6.
 
-dp[i] = dp[i-1]+nums[i]   dp[i-1]>0
-        nums[i]     otherwise
+click to show more practice.
+
+More practice:
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
 */
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        vector<int> dp(nums.size()+1, 0);
-        for (int i = 1; i <= nums.size(); i++)
-            dp[i] = dp[i-1] >= 0 ? dp[i-1] + nums[i] : nums[i];
-        
-        int res = INT_MIN;
-        for (int i = 1; i <= nums.size(); i++)
-            res = max(res, dp[i]);
-        return res;
+        int r = INT_MIN, s = 0;
+        for (int num : nums) {
+            s = s > 0 ? s + num : num;
+            r = max(r, s);
+        }
+        return r;
     }
 };
