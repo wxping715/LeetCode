23f22c25daa5f4dcb485f3c0246e7d2014332027
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-17 14:28:46 +0800

    Update 114.cpp

--- 114.cpp
+++ 114.cpp
@@ -1,3 +1,28 @@
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+*/
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,22 +34,50 @@
  */
 class Solution {
 public:
+    
+    // recursive method
     void flatten(TreeNode* root) {
-        stack<TreeNode*> s;
-        if (!root) return;
-        s.push(root);
-        
+        TreeNode* tail = flattenSub(root);
+        tail = root;
+    }
+    
+    TreeNode* flattenSub(TreeNode* root) {
+        if (!root) return root;
+        if (!root->left && !root->right) return root;
+    
+        TreeNode* ltail = flattenSub(root->left);
+        TreeNode* rtail = flattenSub(root->right);
+        if (!ltail) return rtail;
+        else {
+            ltail->right = root->right;
+            root->right = root->left;
+            root->left = NULL;
+            if (rtail) return rtail;
+            return ltail;
+        }
+    }
+    
+    // iterative solution
+    // preorder
+    void flatten(TreeNode* root) {
         TreeNode* rear = NULL;
+        stack<TreeNode*> s;
+        if (root) s.push(root);
         while (!s.empty()) {
-            TreeNode* cur = s.top();
+            auto cur = s.top();
             s.pop();
+            
             if (cur->right) s.push(cur->right);
             if (cur->left) s.push(cur->left);
             
             cur->left = NULL;
             cur->right = NULL;
-            if (rear) rear->right = cur;
-            rear = cur;
+            
+            if (rear) {
+                rear->right = cur;
+                rear = cur;
+            }
+            else rear = cur;
         }
     }
 };
