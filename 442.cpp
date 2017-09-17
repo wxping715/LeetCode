7c176ad6dca10d1f91be0338b6efb10b097fa547
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-17 15:55:26 +0800

    Update 442.cpp

--- 442.cpp
+++ 442.cpp
@@ -1,11 +1,27 @@
+/*
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements that appear twice in this array.
+
+Could you do it without extra space and in O(n) runtime?
+
+Example:
+Input:
+[4,3,2,7,8,2,3,1]
+
+Output:
+[2,3]
+*/
+
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> res;
-        for (auto i: nums) {
-            int id = abs(i)-1;
-            if (nums[id] < 0) res.push_back(id+1);
-            nums[id] = -nums[id];
+        for (int num : nums) {
+            int idx = abs(num);
+            if (nums[idx-1] < 0) res.push_back(idx);
+            else
+                nums[idx-1] = -nums[idx-1];
         }
         return res;
     }
