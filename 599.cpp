d5f1fb9fdf42cd63424bb7f6600b62efa7740ec3
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-19 20:30:11 +0800

    Create 599.cpp

---
+++ 599.cpp
@@ -0,0 +1,54 @@
+/*
+Suppose Andy and Doris want to choose a restaurant for dinner, and they both have a list of favorite restaurants represented by strings.
+
+You need to help them find out their common interest with the least list index sum. If there is a choice tie between answers, output all of them with no order requirement. You could assume there always exists an answer.
+
+Example 1:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+Output: ["Shogun"]
+Explanation: The only restaurant they both like is "Shogun".
+Example 2:
+Input:
+["Shogun", "Tapioca Express", "Burger King", "KFC"]
+["KFC", "Shogun", "Burger King"]
+Output: ["Shogun"]
+Explanation: The restaurant they both like and have the least index sum is "Shogun" with index sum 1 (0+1).
+Note:
+The length of both lists will be in the range of [1, 1000].
+The length of strings in both lists will be in the range of [1, 30].
+The index is starting from 0 to the list length minus 1.
+No duplicates in both lists.
+*/
+
+class Solution {
+public:
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        vector<string> res;
+        unordered_map<string, int> hashmap;
+        
+        for (int i = 0; i < list1.size(); i++)
+            hashmap[list1[i]] = i;
+        
+        int mini = list1.size() + list2.size();
+        for (int i = 0; i < list2.size(); i++) {
+            if (!hashmap.count(list2[i])) continue;
+            int j = hashmap[list2[i]];
+            if (i+j == mini) res.push_back(list2[i]);
+            else if (i+j < mini) {
+                res.clear();
+                res.push_back(list2[i]);
+                mini = i+j;
+            }
+        }
+        return res;
+    }
+};
+
+// // ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+// // ["KFC", "Shogun", "Burger King"]
+
+
+// res: KFC  mini = 3
+// res: Shogun  mini = 1
