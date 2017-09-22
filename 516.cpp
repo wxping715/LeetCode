36ee52b5d064ca71388dcff208d2fdfa07aa8a10
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-22 23:12:55 +0800

    Update 516.cpp

--- 516.cpp
+++ 516.cpp
@@ -1,27 +1,41 @@
+/*
+Given a string s, find the longest palindromic subsequence's length in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+Input:
+
+"bbbab"
+Output:
+4
+One possible longest palindromic subsequence is "bbbb".
+Example 2:
+Input:
+
+"cbbd"
+Output:
+2
+One possible longest palindromic subsequence is "bb".
+*/
+
 class Solution {
 public:
-
-    /*
-    * dp(i,j) means the longest value from i and j
-    * dp(i,j) = s[i] == s[j] : dp(i+1, j-1)+2 : max(dp(i, j-1), dp(i+1, j))
-    */
-    int longestPalindromeSubseq(string s) {
-        if (s.empty()) return 0;
-        
-        int dp[s.length()][s.length()] = {0};
-        for (int i = 0; i < s.length(); i++) dp[i][i] = 1;
-        for (int i = 0; i+1 < s.length(); i++) dp[i][i+1] = s[i] == s[i+1] ? 2 : 1;
-        
-        for (int l = 3; l <= s.length(); l++)
-            for (int i = 0; i+l <= s.length(); i++) {
-                int j = i+l-1;
-                dp[i][j] = s[i] == s[j] ? dp[i+1][j-1]+2 : max(dp[i][j-1], dp[i+1][j]);
-            }
-        
-        return dp[0][s.length()-1];
-    }
     
-    /*
-    * TODO: optimize spaces
-    */
+    // dp(i, j) = dp(i+1, j-1) + 2, s[i] == s[j]
+    //            max(dp(i+1, j), dp(i, j-1))
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        
+        for (int l = 2; l <= n; l++)
+            for (int i = 0; i+l <= n; i++) {
+                int j = i + l - 1;
+                if (s[i] == s[j]) dp[i][j] = (i+1 < j ? dp[i+1][j-1] : 0) + 2;
+                else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+    
+        if (n == 0) return 0;
+        return dp[0][n-1];
+    }
 };
