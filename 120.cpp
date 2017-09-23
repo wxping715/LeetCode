a2a7f0d40ef4d00f2e8197043232f9a3bf46baa9
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-23 12:01:31 +0800

    Update 120.cpp

--- 120.cpp
+++ 120.cpp
@@ -1,34 +1,40 @@
+/*
+Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+For example, given the following triangle
+[
+     [2],
+    [3,4],
+   [6,5,7],
+  [4,1,8,3]
+]
+The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+
+Note:
+Bonus point if you are able to do this using only O(n) extra space, where n is the total number of rows in the triangle.
+*/
+
 class Solution {
 public:
-
-    int min(int a,int b){
-        return a < b ? a : b;
-    }
-
-    int minimumTotal(vector<vector<int> > &triangle) {
-        vector<int> pre, cur, tmp;
-        
-        if(triangle.size() == 1) return triangle[0][0];
-        
-        pre = triangle[0];
-        for(int i = 1;i < triangle.size();i++)
-        {
-            cur.clear();
-            for(int j = 0;j <= i;j++){
-                if(j == 0) cur.push_back(pre[j] + triangle[i][j]);
-                else if(j == i) cur.push_back(pre[j-1] + triangle[i][j]);
-                else cur.push_back(min(pre[j-1],pre[j])+triangle[i][j]);
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> pre(n, 0), cur(n, 0);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) cur[j] = pre[j];
+                else if (j == i) cur[j] = pre[j-1];
+                else cur[j] = min(pre[j], pre[j-1]);
+                
+                cur[j] += triangle[i][j];
+                
+                // cout << i << " " << j << " " << cur[j] << endl;
             }
-            
-            tmp = pre;
             pre = cur;
-            cur = tmp;
         }
         
-        int res = 99999999, size = triangle.size();
-        for(int i = 0;i < size;i++)
-            res = res > pre[i] ? pre[i] : res;
-        
-        return res;
+        int minv = INT_MAX;
+        for (int i = 0; i < n; i++)
+            minv = min(pre[i], minv);
+        return minv;
     }
 };
