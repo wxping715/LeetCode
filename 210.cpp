3343f00977875d07b8621ddafe9c9986ccf2ae8d
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-25 20:28:35 +0800

    Update 210.cpp

--- 210.cpp
+++ 210.cpp
@@ -1,32 +1,55 @@
+/*
+There are a total of n courses you have to take, labeled from 0 to n - 1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+For example:
+
+2, [[1,0]]
+There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1]
+
+4, [[1,0],[2,0],[3,1],[3,2]]
+There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. So one correct course order is [0,1,2,3]. Another correct ordering is[0,2,1,3].
+
+Note:
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+*/
+
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
-        vector<int> degree(numCourses, 0);
-        // inversed graph
         vector<vector<int>> graph(numCourses, vector<int>());
+        vector<int> degree(numCourses, 0);
         
-        for (vector<pair<int,int>>::iterator it = prerequisites.begin(); it != prerequisites.end(); it++) {
-            graph[it->second].push_back(it->first);
-            degree[it->first] += 1;
+        for (auto p : prerequisites) {
+            graph[p.second].push_back(p.first);
+            degree[p.first] += 1;
         }
         
         vector<int> res;
+        vector<bool> visited(numCourses, false);
         for (int i = 0; i < numCourses; i++) {
             bool found = false;
-            int j;
-            for (j = 0; j < numCourses; j++){
-                if (degree[j] == 0) {
+            int u;
+            for (int j = 0; j < numCourses; j++)
+                if (degree[j] == 0 && !visited[j]) {
+                    u = j;
                     found = true;
                     break;
                 }
-            }
             
-            if (!found) return vector<int>();
-            res.push_back(j);
-            for (int k = 0; k < graph[j].size(); k++)
-                degree[graph[j][k]]--;
-            degree[j] = 0x3f3f3f3f;
+            if (!found) return {};
+            res.push_back(u);
+            visited[u] = true;
+            
+            for (int v : graph[u])
+                degree[v]--;
         }
         return res;
-    }   
+    }
 };
