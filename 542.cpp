662765830ca859a9a566c5ae416e9e7444330755
Xueping Weng <wxping715@users.noreply.github.com>
2017-09-25 13:59:58 +0800

    Update 542.cpp

--- 542.cpp
+++ 542.cpp
@@ -1,29 +1,61 @@
+/*
+Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+
+The distance between two adjacent cells is 1.
+Example 1: 
+Input:
+
+0 0 0
+0 1 0
+0 0 0
+Output:
+0 0 0
+0 1 0
+0 0 0
+Example 2: 
+Input:
+
+0 0 0
+0 1 0
+1 1 1
+Output:
+0 0 0
+0 1 0
+1 2 1
+Note:
+The number of elements of the given matrix will not exceed 10,000.
+There are at least one 0 in the given matrix.
+The cells are adjacent in only four directions: up, down, left and right.
+*/
+
 class Solution {
 public:
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        
+        int n = matrix.size(), m = matrix[0].size();
         queue<pair<int, int>> q;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) q.push(make_pair(i, j));
+        
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) q.emplace(i,j);
                 else matrix[i][j] = INT_MAX;
             }
         
-        int ax[4] = {-1, 0, 1, 0};
-        int ay[4] = {0, 1, 0, -1};
+        int ax[4] = {0, -1, 0, 1};
+        int ay[4] = {-1, 0, 1, 0};
         while (!q.empty()) {
             auto cur = q.front();
             q.pop();
+            
             for (int i = 0; i < 4; i++) {
-                int r = cur.first + ax[i];
-                int c = cur.second + ay[i];
+                int r = ax[i] + cur.first;
+                int c = ay[i] + cur.second;
                 
-                if (r < 0 || r >= m || c < 0 || c >= n ||
-                    matrix[r][c] <= matrix[cur.first][cur.second]+1)
+                if (r < 0 || r >= n || c < 0 || c >= m ||
+                   matrix[r][c] <= matrix[cur.first][cur.second])
                     continue;
-                q.push(make_pair(r,c));
-                matrix[r][c] = matrix[cur.first][cur.second]+1;
+                q.emplace(r, c);
+                matrix[r][c] = matrix[cur.first][cur.second] + 1;
             }
         }
         return matrix;
