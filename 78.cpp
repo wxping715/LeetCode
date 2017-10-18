310ce8697c82107eb658c27977d6ccad24b1ba79
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-18 05:39:49 -0500

    Update 78.cpp

--- 78.cpp
+++ 78.cpp
@@ -1,26 +1,58 @@
+/*
+Given a set of distinct integers, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
 class Solution {
 public:
+    
+    // dfs
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res, fres;
-        vector<int> path;
-        
-        for (int i = 0; i <= nums.size(); i++) {
-            generate(nums, res, path, i, 0);
-            fres.insert(fres.end(), res.begin(), res.end());
-            res.clear();
-        }
-        return fres;
+        vector<vector<int>> result;
+        dfs(nums, {}, result, 0);
+        return result;
     }
     
-    void generate(vector<int>& nums, vector<vector<int>>& res, vector<int>& path, int r, int c) {
-        if (r == 0) {
-            res.push_back(vector<int>(path.begin(), path.end()));
+    void dfs(vector<int>& nums, vector<int> subset, vector<vector<int>>& subsets, int idx) {
+        if (idx == nums.size()) {
+            subsets.push_back(subset);
             return;
         }
-        for (int i = c; i <= nums.size()-r; i++) {
-            path.push_back(nums[i]);
-            generate(nums, res, path, r-1, i+1);
-            path.pop_back();
+        
+        vector<int> left_subset(subset.begin(), subset.end());
+        dfs(nums, left_subset, subsets, idx+1);
+        subset.push_back(nums[idx]);
+        dfs(nums, subset, subsets, idx+1);
+    }
+    
+    // iteration + bit
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        int size = 1 << nums.size();
+        for (int i = 0; i < size; i++) {
+            int k = i;
+            vector<int> subset;
+            for (int j = 0; j < nums.size(); j++) {
+                if (k&1) subset.push_back(nums[j]);
+                k >>= 1;
+            }
+            result.push_back(subset);
         }
+        return result;
     }
 };
