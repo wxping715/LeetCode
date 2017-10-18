82f598cc27e5e5bf4782da0881d26a69061bf6b3
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-18 09:56:19 -0500

    Update 387.cpp

--- 387.cpp
+++ 387.cpp
@@ -1,11 +1,27 @@
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+*/
+
 class Solution {
 public:
     int firstUniqChar(string s) {
-        int freq[26] = {0};
+        int hashset[26] = {0};
+        
+        for (char ch : s)
+            hashset[ch-'a']++;
+        
         for (int i = 0; i < s.length(); i++)
-            freq[s[i]-'a']++;
-        for (int i = 0; i < s.length(); i++)
-            if (freq[s[i]-'a'] == 1) return i;
+            if (hashset[s[i]-'a'] == 1)
+                return i;
         return -1;
     }
 };
