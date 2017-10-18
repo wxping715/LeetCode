7506c3f2c7a6dfedb3aa6583c4c0fc979d083251
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-18 13:13:15 -0500

    Update 119.cpp

--- 119.cpp
+++ 119.cpp
@@ -1,33 +1,22 @@
 /*
-Given numRows, generate the first numRows of Pascal's triangle.
+Given an index k, return the kth row of the Pascal's triangle.
 
-For example, given numRows = 5,
-Return
+For example, given k = 3,
+Return [1,3,3,1].
 
-[
-     [1],
-    [1,1],
-   [1,2,1],
-  [1,3,3,1],
- [1,4,6,4,1]
-]
+Note:
+Could you optimize your algorithm to use only O(k) extra space?
 */
-
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        if (numRows == 0) return {};
-        
-        vector<vector<int>> result;
-        result.push_back({1});
-        for (int i = 2; i <= numRows; i++) {
-            vector<int> cur(i, 0);
-            for (int j = 0; j < i; j++) {
-                if (j == 0) cur[j] = result[i-2][j];
-                else if (j == i-1) cur[j] = result[i-2][j-1];
-                else cur[j] = result[i-2][j] + result[i-2][j-1];
+    vector<int> getRow(int rowIndex) {
+        vector<int> result(rowIndex+1, 1);
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (j == i) result[j] = result[j-1];
+                else if (j != 0)
+                    result[j] = result[j-1] + result[j];
             }
-            result.push_back(cur);
         }
         return result;
     }
