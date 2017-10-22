7081e31f8a3660769dae25a09fdab27478c01e0f
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-23 01:02:58 +0800

    Update 385.cpp

--- 385.cpp
+++ 385.cpp
@@ -1,3 +1,31 @@
+/*
+Given a nested list of integers represented as a string, implement a parser to deserialize it.
+
+Each element is either an integer, or a list -- whose elements may also be integers or other lists.
+
+Note: You may assume that the string is well-formed:
+
+String is non-empty.
+String does not contain white spaces.
+String contains only digits 0-9, [, - ,, ].
+Example 1:
+
+Given s = "324",
+
+You should return a NestedInteger object which contains a single integer 324.
+Example 2:
+
+Given s = "[123,[456,[789]]]",
+
+Return a NestedInteger object containing a nested list with 2 elements:
+
+1. An integer containing value 123.
+2. A nested list containing two elements:
+    i.  An integer containing value 456.
+    ii. A nested list with one element:
+         a. An integer containing value 789.
+*/
+
 /**
  * // This is the interface that allows for creating nested lists.
  * // You should not implement it, or speculate about its implementation
@@ -30,47 +58,25 @@
 class Solution {
 public:
     NestedInteger deserialize(string s) {
-        stack<NestedInteger> nistack;
-        int number = 0, flag = 1;
-        NestedInteger cur;
+        stack<NestedInteger> st;
+        string cur = "";
+        
         for (int i = 0; i < s.length(); i++) {
             char ch = s[i];
-            if (ch == '[') {
-                if (i != 0)
-                    nistack.push(cur);
-                cur = NestedInteger();
-            } else if (ch == ']') {
-                if (isdigit(s[i-1])) {
-                    // cout << number << endl;
-                    NestedInteger ni(number*flag);
-                    cur.add(ni);
-                    number = 0;
-                    flag = 1;
-                }
+            if (ch == '[') st.push(NestedInteger());
+            else if (ch == ',' || ch == ']') {
+                if (cur != "") st.top().add(NestedInteger(stoi(cur)));
+                cur = "";
                 
-                if (!nistack.empty()) {
-                    NestedInteger old = nistack.top();
-                    nistack.pop();
-                    old.add(cur);
-                    cur = old;
+                if (ch == ']') {
+                    auto top = st.top();
+                    st.pop();
+                    if (st.empty()) return top;
+                    else st.top().add(top);
                 }
-            } else if (ch == ',') {
-                if (isdigit(s[i-1])) {
-                    // cout << number << endl;
-                    NestedInteger ni(number*flag);
-                    cur.add(ni);
-                    number = 0;
-                    flag = 1;
-                }
-            } else if (ch == '-') {
-               flag = -1; 
-            } else {
-                number = number*10+(ch-'0');
             }
+            else cur.push_back(ch);
         }
-        
-        if (isdigit(s.back())) cur = NestedInteger(number*flag);
-        return cur;
+        return NestedInteger(stoi(cur));
     }
 };
-
