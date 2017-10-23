6d0e5e3255fa5c5918335ede34056118e7705883
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-23 16:34:03 +0800

    Update 331.cpp

--- 331.cpp
+++ 331.cpp
@@ -1,59 +1,69 @@
 /*
-* using the degree to check whether valid.
-*   1. each node has a indegree
-*   2. no-lead node has two outdegree
-*   3. the final degree is 0
+One way to serialize a binary tree is to use pre-order traversal. When we encounter a non-null node, we record the node's value. If it is a null node, we record using a sentinel value such as #.
+
+     _9_
+    /   \
+   3     2
+  / \   / \
+ 4   1  #  6
+/ \ / \   / \
+# # # #   # #
+For example, the above binary tree can be serialized to the string "9,3,4,#,#,1,#,#,2,#,6,#,#", where # represents a null node.
+
+Given a string of comma separated values, verify whether it is a correct preorder traversal serialization of a binary tree. Find an algorithm without reconstructing the tree.
+
+Each comma separated value in the string must be either an integer or a character '#' representing null pointer.
+
+You may assume that the input format is always valid, for example it could never contain two consecutive commas such as "1,,3".
+
+Example 1:
+"9,3,4,#,#,1,#,#,2,#,6,#,#"
+Return true
+
+Example 2:
+"1,#"
+Return false
+
+Example 3:
+"9,#,#,1"
+Return false
+
+Credits:
+Special thanks to @dietpepsi for adding this problem and creating all test cases.
 */
+
 class Solution {
 public:
+    
+    //stack version
     bool isValidSerialization(string preorder) {
-        //root has no degree
-        int degree = -1;
-        bool is = false;
-        for (int i = 0;i < preorder.length(); i++) {
-            if (preorder[i] == ',') continue;
-            else if (preorder[i] == '#') is = true;
-            else {
-                is = false;
-                while (i+1<preorder.length() && preorder[i+1] >= '0' && preorder[i+1] <= '9') i++;
+        stack<string> st;
+        
+        istringstream is(preorder);
+        string token;
+        while (getline(is, token, ',')) {
+            while (token == "#" && !st.empty() && st.top() == token) {
+                st.pop();
+                if (st.empty()) return false;
+                st.pop();
             }
-            
+            st.push(token);
+        }
+        
+        return st.size() == 1 && st.top() == "#";
+    }
+    
+    //degree version
+    bool isValidSerialization(string preorder) {
+        istringstream is(preorder);
+        string token;
+        
+        int degree = -1;
+        while (getline(is, token, ',')) {
             degree++;
             if (degree > 0) return false;
-            if (!is) degree -= 2;
+            if (token != "#") degree -= 2;
         }
         return degree == 0;
-    }
-};
-
-
-/*
-* using stack top check, each time push the element into the stack.if stack[-1] and stack[-2] are '#', stack[-3] != '#', it means a valid node, then remove those nodes from stack and push the '#' to stack.
-*/
-class Solution {
-public:
-    vector<int> s;
-    bool isValidSerialization(string preorder) {
-        
-        bool is = false;
-        for (int i = 0;i < preorder.length(); i++) {
-            if (preorder[i] == ',') continue;
-            else if (preorder[i] == '#') is = true;
-            else {
-                is = false;
-                while(i+1<preorder.length()&&preorder[i+1]>='0'&&preorder[i+1]<='9') i++;
-            }
-            s.push_back(is);
-            while(s.size() >= 3) {
-                if (s[s.size()-1] && s[s.size()-2] && !s[s.size()-3]) {
-                    s.pop_back();
-                    s.pop_back();
-                    s.pop_back();
-                    s.push_back(true);
-                } else break;
-            }
-        }
-        if (s.size() == 1 && s.front()) return true;
-        return false;
     }
 };
