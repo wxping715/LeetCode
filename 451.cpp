59e740d2e4d2f7fa5f54be196565d8f57fe9f70b
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-23 19:02:18 +0800

    Update 451.cpp

--- 451.cpp
+++ 451.cpp
@@ -1,25 +1,61 @@
+/*
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+Example 2:
+
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+Example 3:
+
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+*/
+
 class Solution {
 public:
     string frequencySort(string s) {
-        int hash[256] = {0};
-        for (char ch : s) hash[ch]++;
-        
-        vector<pair<int, int>> v;
-        for (int i = 0; i < 256; i++) {
-            if (hash[i] != 0)
-                v.emplace_back(i, hash[i]);
+        int hashmap[256] = {0}, maxFreq = 0;
+        for (char ch : s) {
+            hashmap[ch]++;
+            maxFreq = max(maxFreq, hashmap[ch]);
         }
-        sort(v.begin(), v.end(), compare);
+    
+        vector<vector<char>> sortedChars(maxFreq+1, vector<char>());
+        for (int i = 0; i < 256; i++)
+            if (hashmap[i] > 0)
+                sortedChars[hashmap[i]].push_back(i);
         
         string res = "";
-        for (auto p : v) {
-            while (p.second--)
-                res.push_back(p.first);
-        }
+        for (int i = maxFreq; i >= 1; i--)
+            for (char ch : sortedChars[i]) {
+                int k = i;
+                while (k--) res.push_back(ch);
+            }
         return res;
-    }
-    
-    bool static compare(const pair<int, int> &a, const pair<int,int> &b) {
-        return a.second > b.second;
     }
 };
