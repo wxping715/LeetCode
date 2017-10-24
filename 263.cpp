166fcb06f829fa68a8515bfb28e4e7b8e05a233a
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-24 11:24:14 +0800

    Update 263.cpp

--- 263.cpp
+++ 263.cpp
@@ -1,10 +1,23 @@
-bool isUgly(int num) {
-    if (num <= 0) return false;
-    if (num == 1) return true;
-    
-    int f[] = {2,3,5};
-    for(int i = 0;i < 3;i++) {
-        while(num % f[i] == 0) num /= f[i];
+/*
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number.
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num <= 0) return false;
+        
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        
+        return num == 1;
     }
-    return num == 1;
-}
+};
