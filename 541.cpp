61c91413ab4fcfec8c8f8c4e44912fff17a35687
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-25 19:51:29 +0800

    Update 541.cpp

--- 541.cpp
+++ 541.cpp
@@ -1,17 +1,23 @@
+/*
+Given a string and an integer k, you need to reverse the first k characters for every 2k characters counting from the start of the string. If there are less than k characters left, reverse all of them. If there are less than 2k but greater than or equal to k characters, then reverse the first k characters and left the other as original.
+Example:
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"
+Restrictions:
+The string consists of lower English letters only.
+Length of the given string and k will in the range [1, 10000]
+
+*/
 class Solution {
 public:
+    
     string reverseStr(string s, int k) {
-        k = min(k, (int)s.length());
-        int i = 0,j;
-        while (i < s.length()) {
-            j = min(i+k-1, (int)s.length()-1);
-            myReverse(s, i, j);
-            i += 2*k;
+        int l = 0;
+        while (l < s.length()) {
+            int r = min(l+k, (int)s.length());
+            reverse(s.begin()+l, s.begin()+r);
+            l += 2*k;
         }
         return s;
-    }
-    
-    void myReverse(string& s, int i, int j) {
-        while (i <= j) swap(s[i++], s[j--]);
     }
 };
