1fd9107c11713bfb4af9000cb9fafec202ef34ea
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-25 20:59:31 +0800

    Update 14.cpp

--- 14.cpp
+++ 14.cpp
@@ -1,25 +1,26 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+*/
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        if (strs.size() == 1) return strs.front();
         
-        if(strs.size() < 1) return "";
-        if(strs.size() == 1) return strs[0];
-        
-        set<string> myset;
-        for(int i = 1;i <= strs[0].length();i++)
-            myset.insert(strs[0].substr(0,i));
-        int end = strs[0].length();
-        
-        for(int i = 1;i < strs.size();i++){
-            string s = strs[i];
-            
-            if(s.length() < end) end = s.length();
-            
-            while(end > 0 && myset.find(s.substr(0,end)) == myset.end()){
-                end--;
-            }   
+        int s = 0;
+        while (true) {
+            char ch = 0;
+            bool end = false;
+            for (string str : strs) {
+                if (s >= str.length() || (ch != 0 && str[s] != ch)) {
+                    end = true;
+                    break;
+                }
+                ch = str[s];
+            }
+            if (end) break;
+            s++;
         }
-        
-        return strs[0].substr(0,end);
+        return strs.front().substr(0, s);
     }
 };
