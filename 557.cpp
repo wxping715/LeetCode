ab84dd3a94d867a8a1f50db2da24695716adfa0f
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-25 19:46:06 +0800

    Update 557.cpp

--- 557.cpp
+++ 557.cpp
@@ -1,21 +1,22 @@
+/*
+Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+Example 1:
+Input: "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+*/
 class Solution {
 public:
-    string reverseWords(string s) {
-        int si = -1;
+    string reverseWords(string s) {    
+        int l = -1;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] == ' ' || i+1 == s.length()) {
-                if (si != -1) {
-                    reverse(s, si, (s[i] == ' ' ? i-1 : i));
-                    si = -1;
-                }
+                if (l != -1) 
+                    reverse(s.begin()+l, s.begin() + (s[i] == ' ' ? i : i+1));
+                l = -1;
             }
-            else if (si == -1) si = i;
+            else if (l == -1) l = i;
         }
         return s;
-    }
-    
-    void reverse(string& s, int si, int ei) {
-        int i = si, j = ei;
-        while (i <= j) swap(s[i++], s[j--]);
     }
 };
