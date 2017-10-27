59ec0572e2c8d6467d6b02546565e499cd837d65
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-27 21:45:25 +0800

    Update 198.cpp

--- 198.cpp
+++ 198.cpp
@@ -9,18 +9,13 @@ Special thanks to @ifanchu for adding this problem and creating all test cases. 
 
 class Solution {
 public:
-    /*
-    * rob the i-th room or not rob the i-th room
-    * dp(i) = max(dp(i-1), dp(i-2)+nums(i-1)))
-    */
     int rob(vector<int>& nums) {
-        if (nums.size() == 0) return 0;
-        if (nums.size() == 1) return nums[0];
-        
-        int dp[nums.size()+1];
-        dp[0] = 0; dp[1] = nums[0];
-        for (int i = 2;i <= nums.size(); i++)
-            dp[i] = max(dp[i-2]+nums[i-1], dp[i-1]);
-        return dp[nums.size()];
+        int rob = 0, norob = 0;
+        for (int num : nums) {
+            int t = norob;
+            norob = max(rob, norob);
+            rob = t + num;
+        }
+        return max(rob, norob);
     }
 };
