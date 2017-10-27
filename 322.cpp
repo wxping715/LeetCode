34937fc37cb4b7b2193b719fdb9ffd8bf25277cf
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-27 22:50:04 +0800

    Update 322.cpp

--- 322.cpp
+++ 322.cpp
@@ -1,25 +1,44 @@
+/*
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Example 1:
+coins = [1, 2, 5], amount = 11
+return 3 (11 = 5 + 5 + 1)
+
+Example 2:
+coins = [2], amount = 3
+return -1.
+
+Note:
+You may assume that you have an infinite number of each kind of coin.
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
 class Solution {
 public:
     /*
     * dp[n] = min(dp[n-coins[i]], dp[coins[i]) where 0 <= i <= coins.size() and coins[i] <= n
     */
-    int MAX_INT = 0x3fffffff;
     int coinChange(vector<int>& coins, int amount) {
         
-        vector<int> dp(amount+1, MAX_INT);
+        vector<int> dp(amount+1, amount+1);
+        sort(coins.begin(), coins.end());
         
         dp[0] = 0;
-        for (int i = 0; i < coins.size(); i++)
-            if (amount >= coins[i])
-                dp[coins[i]] = 1;
-            
-        for(int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (i < coins[j] || dp[i-coins[j]] == MAX_INT) continue;
-                if (dp[i] > dp[coins[j]] + dp[i-coins[j]]) 
-                    dp[i] = dp[coins[j]] + dp[i-coins[j]];
+        int k = 0;
+        for (int i = 1; i <= amount; i++) {
+            if (k < coins.size() && coins[k] == i) {
+                dp[i] = 1;
+                k++;
+            }
+            else {
+                for (int j = 0; j < k; j++) {
+                    if (dp[i-coins[j]] > amount) continue;
+                    else dp[i] = min(dp[i-coins[j]]+1, dp[i]);
+                }
             }
         }
-        return dp[amount] != MAX_INT ? dp[amount] : -1;
+        return (dp[amount] > amount ? -1 : dp[amount]);
     }
 };
