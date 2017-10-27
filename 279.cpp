253b84f6a9d154b892787d1554b36a5fdf47b0f4
Xueping Weng <wxping715@users.noreply.github.com>
2017-10-27 21:52:45 +0800

    Update 279.cpp

--- 279.cpp
+++ 279.cpp
@@ -1,17 +1,27 @@
+/*
+Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+For example, given n = 12, return 3 because 12 = 4 + 4 + 4; given n = 13, return 2 because 13 = 4 + 9.
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+*/
+
 class Solution {
 public:
+    
     int numSquares(int n) {
-        if (n == 0) return 0;
-        
-        int dp[n+1];
-        memset(dp, 0x3f3f3f3f, sizeof(dp));
-        
-        for (int i = 1;i <= n;i++) {
-            if ((int)sqrt(i)*(int)sqrt(i) == i) dp[i] = 1;
+        vector<int> dp(n+1, n);
+        int k = 1;
+        for (int i = 1; i <= n; i++) {
+            if (k*k == i) {
+                dp[i] = 1;
+                k++;
+            }
             else {
-                for (int j = 1; j*j < i; j++) 
-                    if (dp[i] > dp[j*j] + dp[i-j*j])
-                        dp[i] = dp[j*j] + dp[i-j*j];
+                // k*k > i
+                for (int j = 1; j < k; j++)
+                    dp[i] = min(dp[i], dp[i-j*j]+1);
             }
         }
         return dp[n];
