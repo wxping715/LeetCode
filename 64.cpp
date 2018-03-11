ab7f63e66b883edbb7ffdef33ab2eed7f0d986f8
Xueping Weng <wxping715@users.noreply.github.com>
2018-03-11 18:10:55 +0800

    Update 64.cpp

--- 64.cpp
+++ 64.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
-
     int minPathSum(vector<vector<int>>& grid) {
-       
-       int m = grid.size();
-       if(m < 1) return 0;
-       
-       int n = grid[0].size();
-       if(n < 1) return 0;
-       
-       int dp[m][n];
-       
-       dp[0][0] = grid[0][0];
-       for(int i = 0;i < m;i++)
-           for(int j = 0;j < n;j++)
-           {
-               if(i+j == 0) continue;
-               
-               dp[i][j] = 0x3f3f3f3f;
-               if(i - 1 >= 0) dp[i][j] = dp[i-1][j];
-               if(j - 1 >= 0) dp[i][j] = dp[i][j-1] > dp[i][j] ? dp[i][j] : dp[i][j-1];
-               
-               dp[i][j] += grid[i][j];
-           }
-           
-        return dp[m-1][n-1];
+        if (grid.empty() || grid[0].empty())
+            return 0;
+        
+        int m = grid.size(), n = grid[0].size();        
+        vector<int> pre(n, 0), cur(n, 0);
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) cur[j] = 0;
+                else cur[j] = INT_MAX;
+                
+                if (i > 0) cur[j] = pre[j];
+                if (j > 0) cur[j] = min(cur[j], cur[j-1]);
+                cur[j] += grid[i][j];
+                
+                // cout << i << " " << j << " " << cur[j] <<endl;
+            }
+            pre = cur;
+        }
+        
+        return pre[n-1];
     }
 };
