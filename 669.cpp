db38242bbae78bbf5946b50800578138d18df576
Xueping Weng <wxping715@users.noreply.github.com>
2019-06-15 15:51:22 +0900

    Update 669.cpp

--- 669.cpp
+++ 669.cpp
@@ -56,3 +56,25 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if (root == null) return null;
+        if (root.val >= L && root.val <= R) {
+            root.left = trimBST(root.left, L, R);
+            root.right = trimBST(root.right, L, R);
+            return root;
+        }
+        if (root.val < L) return trimBST(root.right, L, R);
+        else return trimBST(root.left, L, R);
+    }
+}
