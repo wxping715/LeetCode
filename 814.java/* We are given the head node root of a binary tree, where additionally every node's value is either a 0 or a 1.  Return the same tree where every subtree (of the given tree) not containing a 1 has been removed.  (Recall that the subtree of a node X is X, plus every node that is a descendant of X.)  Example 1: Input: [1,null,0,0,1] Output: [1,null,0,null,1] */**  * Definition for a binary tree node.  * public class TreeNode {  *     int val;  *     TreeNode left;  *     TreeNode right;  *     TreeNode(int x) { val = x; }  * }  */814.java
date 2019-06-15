e588e809255ad00148346385324691e26ebcfea0
Xueping Weng <wxping715@users.noreply.github.com>
2019-06-15 21:08:11 +0900

    Create 814.java

---
+++ 814.java/* We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.  Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.  (Recall that the subtree of a node X is X, plus every node that is a descendant of X.)  Example 1: Input: [1,null,0,0,1] Output: [1,null,0,null,1] */**  * Definition for a binary tree node.  * public class TreeNode {  *     int val;  *     TreeNode left;  *     TreeNode right;  *     TreeNode(int x) { val = x; }  * }  */814.java
@@ -0,0 +1,31 @@
+/*
+We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        
+        if (root.left != null || root.right != null || root.val == 1) return root;
+        return null;
+    }
+}
