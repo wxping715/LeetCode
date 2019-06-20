a0e6f2cc05fa7ca13ea887878ac5ed2151d1b135
Xueping Weng <wxping715@users.noreply.github.com>
2019-06-20 19:45:31 +0900

    Create 817.java

---
+++ 817.java
@@ -0,0 +1,52 @@
+/*
+We are given head, the head node of a linked list containing unique integer values.
+
+We are also given the list G, a subset of the values in the linked list.
+
+Return the number of connected components in G, where two values are connected if they appear consecutively in the linked list.
+
+Example 1:
+
+Input: 
+head: 0->1->2->3
+G = [0, 1, 3]
+Output: 2
+Explanation: 
+0 and 1 are connected, so [0, 1] and [3] are the two connected components.
+Example 2:
+
+Input: 
+head: 0->1->2->3->4
+G = [0, 3, 1, 4]
+Output: 2
+Explanation: 
+0 and 1 are connected, 3 and 4 are connected, so [0, 1] and [3, 4] are the two connected components.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> hashset = new HashSet<>();
+        for (int g : G) hashset.add(g);
+        
+        int result = 0;
+        boolean connected = false;
+        ListNode p = head;
+        while (p != null) {
+            if (!hashset.contains(p.val)) connected = false;
+            else if (!connected) {
+                result += 1;
+                connected = true;
+            }
+            p = p.next;
+        }
+        return result;
+    }
+}
