b7920fe6ea6ebf29fa65a72f681938a9d7f39c31
Xueping Weng <wxping715@users.noreply.github.com>
2019-06-20 19:51:42 +0900

    Create 876.java

---
+++ 876.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null) {
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+            }
+        }
+        return slow;
+    }
+}
