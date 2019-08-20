df4a493ee4d38a1ecd0a642bc56a2ce5d846a16a
Xueping Weng <wxping715@users.noreply.github.com>
2019-08-19 21:24:36 -0500

    Update and rename 153.cpp to 153.java

--- 153.java
+++ 153.java
@@ -9,15 +9,13 @@ You may assume no duplicate exists in the array.
 */
 
 class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int l = 0, r = nums.size()-1;
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1, m;
         while (l < r) {
-            int m = (l+r)/2;
-        	// must be increase, decrese
-        	if (nums[m] > nums[r]) l = m+1;
-        	else r = m;
+            m = (r-l)/2 + l;
+            if (nums[m] > nums[r]) l = m + 1;
+            else r = m;
         }
         return nums[l];
     }
-};
+}
